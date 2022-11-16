adf04d5a9dce61927c1b010279bbfbf9bdeada34
valentin leon <110255891+gerardo-valentin@users.noreply.github.com>
2022-11-16 16:33:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const cargarImagen = (entradas, observador) => {
 		if(entrada.isIntersecting){
 			entrada.target.classList.add('visible');
 		} else {
-			entrada.target.classList.remove('visible');
+			//entrada.target.classList.remove('visible');
 		}
 	});
 }
