4772558af4abc709d47c6042fd0771cab13bf2f0
valentin leon <110255891+gerardo-valentin@users.noreply.github.com>
2022-11-16 16:26:40 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const imagen1 = document.getElementById('imagen1');
+const imagen2=document.getElementById('proyecto1');
+const imagen3 = document.getElementById('proyecto2');
+const imagen4 = document.getElementById('proyecto3');
+const imagen5 = document.getElementById('proyecto4');
+const imagen6 = document.getElementById('proyecto5');
+
+// aqui ya le estoy dando los movimientos  o defino la imangen  
+const cargarImagen = (entradas, observador) => {
+	
+	entradas.forEach((entrada) => {
+		if(entrada.isIntersecting){
+			entrada.target.classList.add('visible');
+		} else {
+			entrada.target.classList.remove('visible');
+		}
+	});
+}
+
+// se crea la opcion observador
+const observador = new IntersectionObserver(cargarImagen, {
+	root: null,
+	rootMargin: '0px 0px 0px 0px',
+	threshold: 1.0
+});
+
+observador.observe(imagen1);
+observador.observe(imagen2);
+observador.observe(imagen3);
+observador.observe(imagen4);
+observador.observe(imagen5);
+observador.observe(imagen6);
